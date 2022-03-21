160870585c0f7d40f6391bdd434875b7d8c6cc2d
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-21 14:45:20 -0700

    static words working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import Films from './views/Films/Films';
+import Main from './views/Main/Main';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>This is a header</header>
+      <Main />
+      <Films />
+    </div>
+  );
 }
 
 export default App;
