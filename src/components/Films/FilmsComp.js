ac5419aa611610d58c5db4618322644129ab57a6
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-21 22:25:51 -0700

    film posters rendering

--- src/components/Films/FilmsComp.js
+++ src/components/Films/FilmsComp.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import './FilmsComp.css';
 export default function Films({ films }) {
   // console.log(films);
+
   return (
     <div>
       {films.map((film) => (
         <div key={film.id} className='film'>
           <h3>{film.title}</h3>
+          <img src={`${process.env.PUBLIC_URL}/films/${film.title.replaceAll(' ', '-').toLowerCase()}.jpeg`} />
           <p>Box office total: {film.box_office_total}</p>
           <p>Academy award nominations: {film.academy_award_nominations}</p>
         </div>
