0686e301f6cf98716a4e420d9bb9709191b51e6e
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-21 21:52:30 -0700

    race selection is working

--- src/views/Characters/Characters.js
+++ src/views/Characters/Characters.js
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import CharactersComp from '../../components/Characters/CharactersComp';
+import Dropdown from '../../components/Dropdown/Dropdown';
 import { fetchCharacters } from '../../services/characters';
 
 export default function Characters() {
   const [characters, setCharacters] = useState([]);
+  const [race, setRace] = useState('All');
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchCharacters('All', []);
+      const data = await fetchCharacters(race, []);
       setCharacters(data);
     };
     fetchData();
-  }, []);
+  }, [race]);
 
   return (
     <div>
+      <Dropdown setRace={setRace} race={race} />
       <CharactersComp characters={characters} />
     </div>
   );
