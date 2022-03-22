e9d94aa3a52787cbeecc9b0b82990d1c457cf0ad
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-21 22:05:27 -0700

    search is working

--- src/views/Characters/Characters.js
+++ src/views/Characters/Characters.js
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import CharactersComp from '../../components/Characters/CharactersComp';
 import Dropdown from '../../components/Dropdown/Dropdown';
+import Search from '../../components/Search/Search';
 import { fetchCharacters } from '../../services/characters';
 
 export default function Characters() {
   const [characters, setCharacters] = useState([]);
   const [race, setRace] = useState('All');
+  const [name, setName] = useState('');
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchCharacters(race, []);
+      const data = await fetchCharacters(race, name);
       setCharacters(data);
     };
     fetchData();
-  }, [race]);
+  }, [race, name]);
 
   return (
     <div>
       <Dropdown setRace={setRace} race={race} />
+      <Search {...{ search, setSearch, setName }} />
       <CharactersComp characters={characters} />
     </div>
   );
